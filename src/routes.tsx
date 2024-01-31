a69b454b99b7917a6bb4e75a02acb254aebd150a
Sidnei Farias <sidneijr.77@gmail.com>
2024-01-31 16:46:00 -0300

    feature: add order tracking components to the Dashboard

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,9 +2,9 @@ import { createBrowserRouter } from 'react-router-dom'
 
 import { AppLayout } from './pages/_layouts/app'
 import { AuthLayout } from './pages/_layouts/auth'
-import { Dashboard } from './pages/app/dashboard'
 import { SignIn } from './pages/auth/sign-in'
 import { SignUp } from './pages/auth/sign-up'
+import { Dashboard } from './pages/dashboard/dashboard'
 import { Orders } from './pages/orders/orders'
 
 export const router = createBrowserRouter([
