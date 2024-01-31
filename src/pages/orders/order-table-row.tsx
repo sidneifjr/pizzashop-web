03bfa9942e1468bf26bfab878778b61b26db6fd8
Sidnei Farias <sidneijr.77@gmail.com>
2024-01-31 13:43:50 -0300

    feature: add order-details

--- src/pages/orders/order-table-row.tsx
+++ src/pages/orders/order-table-row.tsx
@@ -1,17 +1,26 @@
 import { ArrowRight, Search, X } from 'lucide-react'
 
 import { Button } from '@/components/ui/button'
+import { Dialog, DialogContent, DialogTrigger } from '@/components/ui/dialog'
 import { TableCell, TableRow } from '@/components/ui/table'
+
+import { OrderDetails } from './order-details'
 
 export function OrderTableRow() {
   return (
     <TableRow>
       <TableCell>
-        <Button variant="outline" size="xs">
-          <Search className="h-3 w-3" />
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button variant="outline" size="xs">
+              <Search className="h-3 w-3" />
 
-          <span className="sr-only">Detalhes do pedido</span>
-        </Button>
+              <span className="sr-only">Detalhes do pedido</span>
+            </Button>
+          </DialogTrigger>
+
+          <OrderDetails />
+        </Dialog>
       </TableCell>
 
       <TableCell className="font-mono text-xs font-medium">
