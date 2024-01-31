26b7f92c6e1728045e02f51311d14e73bbdc55d2
Sidnei Farias <sidneijr.77@gmail.com>
2024-01-31 18:41:15 -0300

    feature(Dashboard): add revenue-chart

--- src/pages/app/dashboard/dashboard.tsx
+++ src/pages/app/dashboard/dashboard.tsx
@@ -4,6 +4,7 @@ import { DailyOrdersAmountCard } from './daily-orders-amount-card'
 import { MonthlyCanceledOrdersAmountCard } from './monthly-canceled-orders-amount'
 import { MonthlyOrdersAmountCard } from './monthly-orders-amount-card'
 import { MonthlyRevenueCard } from './monthly-revenue-card'
+import { RevenueChart } from './revenue-chart'
 
 export function Dashboard() {
   return (
@@ -18,6 +19,10 @@ export function Dashboard() {
           <MonthlyOrdersAmountCard />
           <DailyOrdersAmountCard />
           <MonthlyCanceledOrdersAmountCard />
+        </div>
+
+        <div className="grid grid-cols-9 gap-4">
+          <RevenueChart />
         </div>
       </div>
     </>
