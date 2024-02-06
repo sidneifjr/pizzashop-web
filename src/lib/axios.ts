54f3b2c4e4fea3951809573869e398d490228149
Sidnei Farias <sidneijr.77@gmail.com>
2024-02-05 23:36:23 -0300

    feat(account-menu): add profile loading states

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -4,5 +4,16 @@ import { env } from '@/env'
 
 export const api = axios.create({
   baseURL: env.VITE_API_URL,
-  withCredentials: true, // enables automatic sending of frontend cookies to the backend.
+  withCredentials: true, // Enables automatic sending of frontend cookies to the backend.
 })
+
+/**
+ * Intercepts all requests during development, as to facilitate visualization of loading state.
+ */
+if (env.VITE_ENABLE_API_DELAY) {
+  api.interceptors.request.use(async (config) => {
+    await new Promise((resolve) => setTimeout(resolve, 2000))
+
+    return config
+  })
+}
